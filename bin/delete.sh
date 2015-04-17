dc32b5546679eacb7e7d1c4ec5d18c7f38848a56
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-16 23:46:49 -0500

    parameterize versions

--- bin/delete.sh
+++ bin/delete.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+aws_cpi_version=${aws_cpi_version:-5}
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 
 export PATH=$PATH:$PWD/bin
 
 bosh-init delete redis.yml \
-  assets/bosh-aws-cpi-release-5.tgz
+  assets/bosh-aws-cpi-release-${aws_cpi_version}.tgz
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+redis_version=${redis_version:-9}
+aws_cpi_version=${aws_cpi_version:-5}
+stemcell_version=${stemcell_version:-2830}
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR/..
 
 export PATH=$PATH:$PWD/bin
 
 bosh-init deploy redis.yml \
-  assets/light-bosh-stemcell-2830-aws-xen-ubuntu-trusty-go_agent.tgz \
-  assets/bosh-aws-cpi-release-5.tgz \
-  assets/redis-9.tgz
+  assets/light-bosh-stemcell-${stemcell_version}-aws-xen-ubuntu-trusty-go_agent.tgz \
+  assets/bosh-aws-cpi-release-${aws_cpi_version}.tgz \
+  assets/redis-${redis_version}.tgz
